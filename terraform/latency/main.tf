218aca9397994fd51a2d4a96519753fd36b5f99f
Darin McGill <darin.mcgill@gmail.com>
2017-12-26 17:54:27 -0500

    tweaks

--- terraform/latency/main.tf
+++ terraform/latency/main.tf
@@ -24,8 +24,8 @@ resource "aws_lb_listener_rule" "static" {
 resource "aws_ecs_task_definition" "main" {
   family                = "latency-${var.env_name}"
   container_definitions = "${file("${path.module}/containers.tmpl")}"
-  cpu = "256"
-  memory = "512"
+  cpu = "128"
+  memory = "128"
   //task_role_arn = "arn:aws:iam::401701269211:role/ecsTaskExecutionRole"
   network_mode = "host"
 }
